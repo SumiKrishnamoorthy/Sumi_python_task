d14d7eb61487d9d31afcace68f343fea70ca517a
sumikrishnamoorthy <sumikrishnamoorthy21@gmail.com>
2023-06-19 19:47:41 +0530

    first commit

---
+++ forloop.py
@@ -0,0 +1,17 @@
+##tabels Using for loop
+##a= int(input ("What tables you want:-"))
+#n=int(input("Times:-"))
+##for i in range(1,n):
+   ## print(a, "X", i,"=", a*i) 
+   ##print(f"{a}*{i}={a*i}")
+    
+##for i in range(1,3):
+  ##  print("0" *1 ,"0" *1 ,   "0"*1,"0"*1)
+    
+for i in range(1,5,1):
+    print("4"*2)
+    
+    
+          
+    
+    
